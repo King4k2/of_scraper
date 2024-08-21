417783c6bbcc864c18958ef7a89f4960bbdf6983
Nikita <laim51709@gmail.com>
2024-08-21 13:22:47 +0400

    auth done

---
+++ config.py
@@ -0,0 +1,5 @@
+api_id = ""
+api_hash = ""
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+sec_ch_ua = '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"'
+cookie = "_cfuvid=eyKnpxqVlHoVem95f9h6UCNi9M2bUX3WGa0XJBfMpyA-1723826341322-0.0.1.1-604800000; fp=8af30e36af2e89401881ef48cb8ea0944ab3e71a; csrf=9LKkzHR7deb6929e7ac95e3da1dffd03418033dd; lang=ru; cookiesAccepted=all; __cf_bm=DGP19Tb68rBJmXY6RULndh1ToZvCUXZbLw_9tVKDHDo-1723983963-1.0.1.1-sYjJ1ZJlaY8dBUiBLit7DVWqKtxQGxe2_vk.maX9cSQ05rx0EcndD1VI3gv_od5dHN6o6SoYj_w8xrJLPFF86g; sess=i1j48o2qsevb8fgffd60avrcp9; auth_id=404158180; ref_src=https%3A%2F%2Fonlyfans.com%2Fposts%2Fcreate; st=423b93cb2383f56d1f26ea00c7a546aaa7c4c6622d0c190d60fbcf3fab26bb9e"
